83905245e099127296ea9b82c5997e727cad85bd
denis200 <soundze@mail.ru>
2022-03-27 11:06:52 +0300

    feat: add rating model and serializer

--- apps/ratings/admin.py
+++ apps/ratings/admin.py
@@ -1,3 +1,9 @@
+from csv import list_dialects
 from django.contrib import admin
+from .models import Rating
 
-# Register your models here.
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ["rater","agent","rating"]
+
+
+admin.site.register(Rating,RatingAdmin)
